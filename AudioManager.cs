f16506d1ffd1d8d54f6067931119c06389b18a18
Luan Mantelo Bonfim <luan_mantelo@hotmail.com>
2018-12-25 23:05:08 -0200

    Audio Manager
    Script responsible for managing all the audios in the game.

---
+++ AudioManager.cs
@@ -0,0 +1,26 @@
+﻿//Script responsible for managing all the audios in the game.
+using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour {
+
+    public Sound[] sounds;
+
+     void Awake()
+    {
+      foreach (Sound s in sounds)
+        {
+            s.Source = gameObject.AddComponent<AudioSource>();
+            s.Source.clip = s.clip;
+            s.Source.volume = s.volume;
+            s.Source.pitch = s.pitch;
+        }
+    }
+
+    public void play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.Source.Play();
+    }
+}

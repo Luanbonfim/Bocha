323d541552132effc647c40c6d3212add85fc645
Luan Mantelo Bonfim <luan_mantelo@hotmail.com>
2018-12-25 22:20:43 -0200

    Buffs feedbacks (Text on the UI)
    Script responsible to manage the feedbacks (texts) on the UI, when any buff is triggered.

---
+++ BuffsFeedBack.cs
@@ -0,0 +1,46 @@
+﻿//Script responsible to manage the feedbacks (texts) on the UI, when any buff is triggered.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuffsFeedBack : MonoBehaviour {
+
+    Text FeedBack;
+    public Text FeedBackText;
+
+    private void Start()
+    {
+        FeedBack = GetComponent<Text>();
+    }
+
+    public void BlueBuff()
+    {
+        FeedBack.text = "BLUE BUFF ACTIVATED, " +
+            "Enemy's Strength Bar Is Now Crazy";
+        FeedBackText.color = Color.blue;
+    }
+
+    public void YellowBuff()
+    {
+        FeedBack.text = "YELLOW BUFF ACTIVATED, " +
+            "Score Doubled";
+        FeedBackText.color = Color.yellow;
+
+    }
+
+    public void BlackBuff()
+    {
+        FeedBack.text = "BLACK BUFF ACTIVATED, " +
+            "Destroying Enemy's Balls";
+        FeedBackText.color = Color.black;
+    }
+
+    public IEnumerator DestroyFeedBack()
+    {
+        yield return new WaitForSeconds(1);
+        FeedBack.text = "";
+    }
+
+
+}

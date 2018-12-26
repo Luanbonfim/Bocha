8c6f4b4cfbbaab8caa7a86a24fce4b452a2bbbb5
Luan Mantelo Bonfim <luan_mantelo@hotmail.com>
2018-12-25 22:22:41 -0200

    Distance from white ball (text on the UI).
    Script responsible to manage the text showing the distance between the current player and the white ball (text on the UI).

---
+++ DistanceText.cs
@@ -0,0 +1,26 @@
+﻿//Script responsible to manage the text showing the distance between the current player and the white ball (text on the UI).
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DistanceText : MonoBehaviour
+{
+
+    public static int distance = 0;
+    Text distanceFromWhiteBall;
+    public Transform blueTeam;
+    public Text distanceText;
+
+    private void Start()
+    {
+        distanceFromWhiteBall = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if (R_Balls.r_ballsValue != 0)
+        {
+            distanceFromWhiteBall.text = distance + "m";
+        }
+
+    }
+}

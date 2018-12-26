59e61dfea75341f4142b37d881d061103d009bc4
Luan Mantelo Bonfim <luan_mantelo@hotmail.com>
2018-12-25 22:23:49 -0200

    Stage Selection Menu
    Script responsible to manage the "stage selection" menu.

---
+++ Fields_Menu.cs
@@ -0,0 +1,38 @@
+﻿//Script responsible to manage the "stage selection" menu.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fields_Menu : MonoBehaviour
+{
+
+    public void LoadField1()
+    {
+        SceneManager.LoadScene("Field1");
+    }
+    public void LoadField2()
+    {
+        SceneManager.LoadScene("Field2");
+    }
+    public void LoadField3()
+    {
+        SceneManager.LoadScene("Field3");
+    }
+    public void LoadMainManu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void PlayAudioSelect()
+    {
+        FindObjectOfType<AudioManager>().play("Select");
+    }
+    public void PlayAudioClick()
+    {
+        FindObjectOfType<AudioManager>().play("Click");
+    }
+}

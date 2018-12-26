b9b08312aa90dcd426c81903a7bf54a45da17218
Luan Mantelo Bonfim <luan_mantelo@hotmail.com>
2018-12-25 22:05:34 -0200

    Blue team balls management
    Script responsible to manage the blue team balls.

---
+++ B_Balls.cs
@@ -0,0 +1,24 @@
+﻿//Script responsible to manage the blue team balls.
+using UnityEngine;
+using UnityEngine.UI;
+public class B_Balls : MonoBehaviour
+{
+
+    public static int b_ballsValue = 6;
+    Text b_Balls;
+    public Transform blueTeam;
+    public Text scoreText;
+
+    private void Start()
+    {
+        b_Balls = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+
+        b_Balls.text = "" + b_ballsValue;
+
+    }
+}
+

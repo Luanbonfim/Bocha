4396548d3282bb9ccd8d5e46137606e478d8589f
Luan Mantelo Bonfim <luan_mantelo@hotmail.com>
2018-12-25 22:28:47 -0200

    Red Team score's feedback.
    Script responsible for managing the red team score's feedback (Text on the UI and audio).

---
+++ R_ScoreFeedBack.cs
@@ -0,0 +1,54 @@
+﻿//Script responsible for managing the red tem score's feedback (Text on the UI and audio).
+using UnityEngine;
+using UnityEngine.UI;
+public class R_ScoreFeedBack : MonoBehaviour
+{
+
+    public static int r_scoreFeedBack = 0;
+    Text r_Score;
+    public Text scoreText;
+    private bool playIncrement;
+    private bool playDecrement;
+
+    private void Start()
+    {
+        r_Score = GetComponent<Text>();
+    }
+    private void Update()
+    {
+        PlayAudio();
+    }
+
+    public void ChangeScore()
+    {
+        r_Score.text = "+ " + r_scoreFeedBack;
+        playIncrement = true;
+    }
+
+    public void DecrementScore()
+    {
+        r_Score.text = "- " + r_scoreFeedBack;
+        playDecrement = true;
+    }
+
+    public void ChangeScoreBack()
+    {
+        r_Score.text = "";
+    }
+
+    private void PlayAudio()
+    {
+        if (playIncrement == true)
+        {
+            FindObjectOfType<AudioManager>().play("Score");
+            playIncrement = false;
+        }
+
+        if (playDecrement == true)
+        {
+            FindObjectOfType<AudioManager>().play("DecrementScore");
+            playDecrement = false;
+        }
+
+    }
+}

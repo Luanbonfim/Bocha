e710457d703ddddcc9c1b7015ea57960604a931a
Luan Mantelo Bonfim <luan_mantelo@hotmail.com>
2018-12-25 22:26:16 -0200

    Red team balls
    Script responsible for managing the red team balls.

---
+++ R_Balls.cs
@@ -0,0 +1,24 @@
+﻿//Script responsible for managing the red team balls
+using UnityEngine;
+using UnityEngine.UI;
+public class R_Balls : MonoBehaviour
+{
+
+    public static int r_ballsValue = 6;
+    Text r_Balls;
+    public Transform redTeam;
+    public Text scoreText;
+
+    private void Start()
+    {
+        r_Balls = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+
+        r_Balls.text = "" + r_ballsValue;
+
+    }
+}
+

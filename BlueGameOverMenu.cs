b49ce5ccf51327b03b96f433d8066b6debcf255f
Luan Mantelo Bonfim <luan_mantelo@hotmail.com>
2018-12-25 22:16:59 -0200

    Game over menu (when the blue team wins)
    Script responsible to manage the game over menu (when the blue team wins).

---
+++ BlueGameOverMenu.cs
@@ -0,0 +1,33 @@
+﻿//Script responsible to manage the game over menu (when the blue team wins).
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BlueGameOverMenu : MonoBehaviour
+{
+    Text b_Score;
+    private void Start()
+    {
+        b_Score = GetComponent<Text>();
+    }
+    public void Update()
+    {
+        b_Score.text = "" + B_Score.b_scoreValue;
+        Debug.Log("Score: " + B_Score.b_scoreValue);
+    }
+    public void PlayAgain()
+    {
+        R_Balls.r_ballsValue = 6;
+        B_Balls.b_ballsValue = 6;
+        B_Score.b_scoreValue = 0;
+        R_Score.r_scoreValue = 0;
+        SceneManager.LoadScene("Menu_Fields");
+      
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}

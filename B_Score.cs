5205bc86f9b505c040ce5d288dc8ea219485dbd0
Luan Mantelo Bonfim <luan_mantelo@hotmail.com>
2018-12-25 22:06:41 -0200

    Blue team score management.
    Script responsible to manage the blue team score.

---
+++ B_Score.cs
@@ -0,0 +1,21 @@
+﻿//Script responsible to manage the blue team score.
+using UnityEngine;
+using UnityEngine.UI;
+public class B_Score : MonoBehaviour {
+
+    public static int b_scoreValue = 0;
+    Text b_Score;
+    public Transform blueTeam;
+    public Text scoreText;
+
+    private void Start()
+    {
+        b_Score = GetComponent<Text>();
+    }
+
+    void Update () {
+
+        b_Score.text = "" + b_scoreValue;
+		
+	}
+}

72b5d5e6f2e3d8c1261443dfd8badab0ebfe5fac
Luan Mantelo Bonfim <luan_mantelo@hotmail.com>
2018-12-25 22:14:00 -0200

    Blue buff (trigger management)
    Script responsible to manage the blue buff, when it is triggered by any ball.

---
+++ BlueBuff_Trigger.cs
@@ -0,0 +1,55 @@
+﻿//Script responsible to manage the blue buff, when it is triggered by any ball.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueBuff_Trigger : MonoBehaviour {
+    public static bool BlueBuffOn;
+    public static bool BlueTriggered;
+    // public GameObject BlueBuffObj;
+
+    private void Start()
+    {
+        StartCoroutine(DestroyBuffs());
+    }
+
+    private void OnTriggerEnter(Collider ball)
+    {
+        
+        if (ball.gameObject.tag == "BlueBall" || ball.gameObject.tag == "RedBall")
+        {
+            BlueTriggered = true;
+            BlueBuffOn = true;
+            if (Buff_Spawner.bluePosi2 == true)
+            {
+                Destroy(Buff_Spawner.BlueBuffClone[2]);
+                Destroy(Buff_Spawner.BlueTriggerClone[2]);
+            }
+            Destroy(Buff_Spawner.BlueBuffClone[Buff_Spawner.j]);
+            Destroy(Buff_Spawner.BlueTriggerClone[Buff_Spawner.j]);
+          //  Debug.Log("BALL TRIGGERED THE BUFF!!!");
+           // Debug.Log("BLUE DESTROYED");
+            Buff_Spawner.j--;
+        }
+    }
+
+    IEnumerator DestroyBuffs()
+    {
+        yield return new WaitForSeconds(20);
+            if (Buff_Spawner.bluePosi2 == true)
+            {
+                Destroy(Buff_Spawner.BlueBuffClone[2]);
+                Destroy(Buff_Spawner.BlueTriggerClone[2]);
+            }
+            Destroy(Buff_Spawner.BlueBuffClone[Buff_Spawner.j]);
+            Destroy(Buff_Spawner.BlueTriggerClone[Buff_Spawner.j]);
+        //  Debug.Log("BALL TRIGGERED THE BUFF!!!");
+        // Debug.Log("BLUE DESTROYED");
+        Buff_Spawner.j--;       
+
+    }
+
+
+
+}
+

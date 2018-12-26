f016fa041ba16ecd805276964c19b2e300a510b5
Luan Mantelo Bonfim <luan_mantelo@hotmail.com>
2018-12-25 22:27:24 -0200

    Red team score (text on the UI).
    Script Responsible for managing the red team score (text on the UI).

---
+++ R_Score.cs
@@ -0,0 +1,23 @@
+﻿//Script Responsible for managing the red team score (text on the UI).
+using UnityEngine;
+using UnityEngine.UI;
+public class R_Score : MonoBehaviour
+{
+
+    public static int r_scoreValue = 0;
+    Text r_Score;
+    public Transform blueTeam;
+    public Text scoreText;
+
+    private void Start()
+    {
+        r_Score = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+
+        r_Score.text = "" + r_scoreValue;
+
+    }
+}

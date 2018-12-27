104b97df67a3e0024e87ad11a7011b36bcbeaeed
Luan Mantelo Bonfim <luan_mantelo@hotmail.com>
2018-12-27 19:18:57 -0200

    Sound (Setup)
    Script responsible to set the audio features that will be shown on the inspector to setup all the audios from the Audio Manager.

---
+++ Sound.cs
@@ -0,0 +1,19 @@
+﻿//Script responsible to set the audio features that will be shown on the inspector to setup all the audios from the Audio Manager.
+using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound {
+
+    public string name;
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+
+    [Range(.1f, 3f)]
+    public float pitch;
+    
+    [HideInInspector]
+    public AudioSource Source;
+}
